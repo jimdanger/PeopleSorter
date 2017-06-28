d7e4ce3ba0076ba09f24f57aa393bcdfa7412751
Jim Wilson <jamesmattheywilson@gmail.com>
2017-06-28 18:57:22 -0400

    Initial Commit

---
+++ PeopleSorter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PeopleSorter
+//
+//  Created by Jim Wilson on 6/28/17.
+//  Copyright © 2017 Jim Danger, LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
